433dd9706d5cd6fdb1897efcca7b02c096e18ad7
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-21 09:10:16 -0500

    heroku push

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const passport = require("passport");
 const routes = require("./routes/index");
 const db = require("./config/keys").mongoURI;
+const path = require("path")
 
 //START EXPRESS
 const app = express();
@@ -16,20 +17,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// // MONGOOSE
-// mongoose
-//     .connect(
-//         db,
-//         { useNewUrlParser: true }
-//     )
-//     .then(() => console.log("MongoDB successfully connected"))
-//     .catch(err => console.log(err));
+// MONGOOSE
+mongoose
+    .connect(
+        db,
+        { useNewUrlParser: true }
+    )
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch(err => console.log(err));
 
 // HEROKU TESTING
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ridonkStories";
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ridonkStories";
 
-mongoose.connect(MONGODB_URI);
+// mongoose.connect(MONGODB_URI);
 
 // PASSPORT
 app.use(passport.initialize());
@@ -38,12 +39,20 @@ require("./config/passport")(passport);
 //ROUTES
 app.use(routes);
 
-//HEROKU
+// //HEROKU
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+// }
+
+// Serve static assets in production
 if (process.env.NODE_ENV === "production") {
+    // Set static folder
     app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
 }
-
-
 
 
 // START SERVER WITH PORT
