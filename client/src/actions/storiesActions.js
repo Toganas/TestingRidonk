48627a6a9c8d75e4c52a32a9d032fd1ab649d29b
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-26 08:21:49 -0500

    for heroku

--- client/src/actions/storiesActions.js
+++ client/src/actions/storiesActions.js
@@ -5,7 +5,7 @@ import history from "../history";
 export const createStories = formValues => {
     return async (dispatch, getState) => {
         const { user } = getState().auth;
-        const response = await axios.post("/api/story/", { ...formValues, user });
+        const response = await axios.post("https://ridonk-stories.herokuapp.com/api/story/", { ...formValues, user });
         dispatch({ type: CREATE_STORIES, payload: response.data })
     }
 };
@@ -13,22 +13,22 @@ export const createStories = formValues => {
 export const fetchStories = () => {
     return async dispatch => {
 
-        const response = await axios.get("/api/story/")
+        const response = await axios.get("https://ridonk-stories.herokuapp.com/api/story/")
         dispatch({ type: FETCH_STORIES, payload: response.data });
     }
 };
 
 export const findByCat = cat => {
     return async dispatch => {
-        const response = await axios.get(`api/story/${cat}`);
+        const response = await axios.get(`https://ridonk-stories.herokuapp.com/api/story/${cat}`);
         dispatch({ type: FIND_BY_CAT, payload: response })
     }
 }
 
-export const deleteStories = id => {
-    return async dispatch => {
-        await axios.delete(`/api/story/${id}`);
-        dispatch({ type: DELETE_STORIES, payload: id });
-        history.push("/")
-    }
-}
+// export const deleteStories = id => {
+//     return async dispatch => {
+//         await axios.delete(`/api/story/${id}`);
+//         dispatch({ type: DELETE_STORIES, payload: id });
+//         history.push("/")
+//     }
+// }
