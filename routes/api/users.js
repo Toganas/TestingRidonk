43130319fa9189b610256bb484936b4b52b97ae3
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-09-04 10:30:35 -0500

    separate the authentication out from the user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,11 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
-
-// Load input validation
-const validateRegisterInput = require("../../validation/register");
-const validateLoginInput = require("../../validation/login");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 // Load User model
 const User = require("../../models/User");
@@ -14,40 +11,78 @@ const User = require("../../models/User");
 // @route POST api/users/register
 // @desc Register user
 // @access Public
-router.post("/register", (req, res) => {
+router.post("/register",
     // Form validation
-
-    const { errors, isValid } = validateRegisterInput(req.body);
-
-    // Check validation
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-
-    User.findOne({ email: req.body.email }).then(user => {
-        if (user) {
-            return res.status(400).json({ email: "Email already exists" });
-        } else {
-            const newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                password: req.body.password
-            });
-
-            // Hash password before saving in database
-            bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if (err) throw err;
-                    newUser.password = hash;
-                    newUser
-                        .save()
-                        .then(user => res.json(user))
-                        .catch(err => console.log(err));
-                });
-            });
+    [
+        check("name", "Name is required")
+            .not()
+            .isEmpty(),
+        check("email", "Please include a valid email")
+            .isEmail(),
+        check(
+            "password",
+            "Please enter a password with 6 or more characters")
+            .isLength({ min: 6 })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            })
         }
+
+        const { name, email, password } = req.body
+
+        try {
+
+            // Does user exist?
+
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res.stats(400).json({ errors: [{ msg: "Email already exists" }] })
+            }
+
+            // Create new user
+
+            user = new User({
+                name,
+                email,
+                password
+            })
+
+            // Encrypt password
+
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            // Return jsonWebToken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get("jwtSecret"),
+                // Expire in 1 year
+                { expiresIn: 31556926 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server error")
+        }
+
     });
-});
 
 // @route POST api/users/login
 // @desc Login user and return JWT token
@@ -106,4 +141,3 @@ router.post("/login", (req, res) => {
 });
 
 module.exports = router;
-
