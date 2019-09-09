b877916f2892ea8bcb4e0c1b5e10dbe3c9fe95cb
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-09-09 11:17:33 -0500

    created routes to post story, find all stories, and find a single story by id

--- routes/api/story.js
+++ routes/api/story.js
@@ -1,18 +1,98 @@
 const router = require("express").Router();
-const storyController = require("../../controllers/storyController")
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
-router.route("/")
-    .get(storyController.findAll)
-    .post(storyController.create);
+// Require Models
+const Story = require("../../models/Story");
+const User = require("../../models/User");
+
+// @route   POST api/story
+// @desc    Create a story
+// @access  Private
+router.post("/", [auth,
+    [
+        check("text", "Text is required")
+    ]
+],
+    async (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        try {
+            const newStory = new Story({
+                story: req.body.story,
+                title: req.body.title
+            })
+
+            const story = await newStory.save();
+
+            res.json(story);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server Error")
+        }
+    }
+);
+
+// @route   GET api/story
+// @desc    Get all stories
+// @access  Public
+router.get("/", async (req, res) => {
+    try {
+        const stories = await Story.find().sort({ date: -1 });
+        res.json(stories)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error")
+    }
+});
+
+// @route   GET api/story/:id
+// @desc    GET story by id
+// @access  Public
+router.get("/:id", async (req, res) => {
+    try {
+        const story = await Story.findById(req.params.id);
+
+        if (!story) {
+            return res.status(404).json({ msg: "Story not found" })
+        }
+
+        res.json(story);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === "ObjectId") {
+            return res.status(404).json({ msg: "Story not found" })
+        }
+        res.status(500).send("Server Error")
+    }
+});
+
+/////////////////////
+// BEGIN ORIGINAL STORY ROUTE
+////////////////////
+// const storyController = require("../../controllers/storyController")
+
+// router.route("/")
+//     .get(storyController.findAll)
+//     .post(storyController.create);
 
 
-// api/story/:cat
-// find all stories with category
-router.route("/category/:cat")
-    .get(storyController.findCat);
+// // api/story/:cat
+// // find all stories with category
+// router.route("/category/:cat")
+//     .get(storyController.findCat);
 
-// find a single story
-router.route("/top")
-    .get(storyController.findOne);
+// // find a single story
+// router.route("/top")
+//     .get(storyController.findOne);
+
+/////////////////////
+// END ORIGINAL STORY ROUTE
+////////////////////
+
+
 
 module.exports = router;
